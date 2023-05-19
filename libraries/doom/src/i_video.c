c84792965c2eb59cb83f442ffd4b980df5cc03a8
Martino Facchin <m.facchin@arduino.cc>
2023-05-19 09:41:33 +0200

    video: restore getNextFrameBuffer

--- libraries/doom/src/i_video.c
+++ libraries/doom/src/i_video.c
@@ -266,6 +266,7 @@ void I_FinishUpdate (void)
     uint8_t* inbuf = I_VideoBuffer;
     uint8_t* outbuf = I_VideoBuffer_FB;
 
+    // this function simply doubles the resolution
     for (uint32_t i = 0; i < SCREENHEIGHT; i++) {
         uint8_t* inbuf_temp = inbuf;
         uint8_t* outbuf_temp = outbuf;
